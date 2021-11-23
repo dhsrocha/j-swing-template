54bbe763a532f4a19122f98c129ccec283da5864
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2021-11-23 02:06:02 +0000

    Bare-bones working JavaFX/Swing assets.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Java Swing Template
+
+A case of study to ensemble all technologies to develop a Java desktop
+application which uses Java Swing and FX altogether.
+
+## Dependencies
+
+* [Lombok](https://projectlombok.org)
+* [SLF4J+Log4j2](https://logging.apache.org/log4j/2.x/log4j-slf4j-impl)
+* [JavaFX](https://openjfx.io)
+* [Java Swing](https://docs.oracle.com/javase/tutorial/uiswing)
+
+## Commands
+
+* `make help`: Displays run instructions. Default target ran.
+* `make build`: Builds a jar bundle from application.
+* `make run`: Builds a jar and runs it.
--- src/main/java/com/dhsrocha/app/App.java
+++ src/main/java/com/dhsrocha/app/App.java
@@ -1,13 +1,43 @@
 package com.dhsrocha.app;
 
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.*;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 @Slf4j
-public final class App {
+public final class App extends Application {
 
-  // TODO Inversion of control.
+  interface Entry {
+    static void main(final String[] args) {
+      App.main(args);
+    }
+  }
 
   public static void main(final String[] args) {
-    log.info("App running. [ {} ]", String.join(" ", args));
+    // TODO Hot-reload
+    launch(args);
+  }
+
+  @Override
+  public void start(final Stage stage) {
+    stage.setTitle("Swing in JavaFX Sample.");
+    stage.setScene(scene());
+    stage.show();
+  }
+
+  // TODO Make it as an injectable component.
+  static final String TESTABLE_BUTTON = "testBtn";
+  private static Scene scene() {
+    val btn = new JButton("Click me!");
+    btn.putClientProperty("id", TESTABLE_BUTTON);
+
+    val swing = new SwingNode();
+    SwingUtilities.invokeLater(() -> swing.setContent(btn));
+    return new Scene(new StackPane(swing), 250, 150);
   }
 }
